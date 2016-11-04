ac84471187b6d9793dd46cf2bb1bc73c0ca75345
Stephen Christopher <random4444@gmail.com>
2016-11-04 12:01:06 -0700

    Add a button and change constraints around when tapped.
    Note: this will disappear the button if you keep tapping.  Weee!
    Challenge: fix that.

--- SquishyBoxes/SquishyBoxes/SquishyBoxes/ViewController.swift
+++ SquishyBoxes/SquishyBoxes/SquishyBoxes/ViewController.swift
@@ -4,6 +4,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var bnrLogoHeight: NSLayoutConstraint!
+
+    @IBAction func didTapChangeStuff(_ sender: UIButton) {
+        let newConstant: CGFloat
+        switch bnrLogoHeight.constant {
+        case 40.0:
+            newConstant = 120.0
+        case 120.0:
+            newConstant = 80.0
+        case 80.0:
+            newConstant = 400.0
+        case 400.0:
+            newConstant = 40.0
+        case _:
+            print("Somehow got to a weird number")
+            newConstant = 40.0
+        }
+
+        bnrLogoHeight.constant = newConstant
+        UIView.animate(withDuration: 2, animations: {
+            self.view.layoutIfNeeded()
+        }    )
+    }
+
 
 }
 
